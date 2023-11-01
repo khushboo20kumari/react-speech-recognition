f0377b2314debba203a6e2426a6f273e20fde0c3
khushboo <khushboo20@navgurukul.org>
2023-11-01 12:34:53 +0530

    campleted this app

--- src/SpeechReco.js
+++ src/SpeechReco.js
@@ -1,7 +1,8 @@
 
 import useClipboard from "react-use-clipboard"
-import React from 'react';
+// import React, { useState } from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+
 
 function SpeechReco() {
 
@@ -13,28 +14,37 @@ function SpeechReco() {
         SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
     }
 
+
+
     return (
         <>
 
 
+            <div className="container-fluid">
+                <div className="card box">
+                    <center>
 
-            <div className="card">
-                <center>
+                        <div className="card-body">
 
-                    <div className="card-body">
-                        <div className="box">
-                            <textarea value={transcript} className="textarea" placeholder="Text Here...."/><br></br>
-                            <button className="btn btn-success" onClick={handleStart}>Start</button>
-                            <button style={{ marginLeft: "10px" }} className="btn btn-danger" onClick={resetTranscript}>Clear</button>
-                            <button style={{ marginLeft: "10px" }} className="btn btn-danger" onClick={stopListening}>Stop</button>
-                            <button style={{ marginLeft: "10px" }} className="btn btn-light" onClick={setCapy}>copy</button>
-                            {capy ? "Yes! 👍" : "Nope! 👎"}
+                            <p style={{ color: "white", fontSize: "30px" }} >Speech Recognition App</p>
+
+                            <div className="box">
+                                <textarea value={transcript} className="textarea" placeholder="Text Here....❤️ " /><br></br>
+
+                                <button className="btn btn-outline-success" onClick={handleStart} style={{ marginTop: "30px" }}>Start</button>
+                                <button style={{ marginLeft: "10px", marginTop: "30px" }} className="btn btn-outline-danger" onClick={resetTranscript}>Clear</button>
+                                <button style={{ marginLeft: "10px", marginTop: "30px" }} className="btn btn-outline-info" onClick={stopListening}>Stop</button>
+                                <button style={{ marginLeft: "10px", marginTop: "30px" }} className="btn btn-outline-warning" onClick={setCapy}>copy</button>
+                                <p style={{ marginTop: "20px", color: "white" }}>{capy ? "Yes! 👍" : "Nope! 👎"}</p>
+                                <p style={{ color: "white" }}>Khushboo kumari...📋 ❣️❣️</p>
+                            </div>
+
+
+
                         </div>
+                    </center>
+                </div>
 
-
-
-                    </div>
-                </center>
             </div>
 
 
